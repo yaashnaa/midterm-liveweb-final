2171ae01d4dc21b122557500af93293c7fd512ef
yg2348 <yg2348@nyu.edu>
2023-10-23 12:13:48 -0400

    Updates

---
+++ precache-manifest.148e9f2e4bd9747179dceb92ab9ada5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c1de36819091f8b32510a2933996b1",
+    "url": "/midterm-liveweb-final/index.html"
+  },
+  {
+    "revision": "7f1204c3ef652c665110",
+    "url": "/midterm-liveweb-final/static/css/2.88034ae4.chunk.css"
+  },
+  {
+    "revision": "263d344f10a4fbcb5081",
+    "url": "/midterm-liveweb-final/static/css/main.0b80e8fb.chunk.css"
+  },
+  {
+    "revision": "7f1204c3ef652c665110",
+    "url": "/midterm-liveweb-final/static/js/2.099d0334.chunk.js"
+  },
+  {
+    "revision": "d7de74e4a7a5eb5adb577e18470a1640",
+    "url": "/midterm-liveweb-final/static/js/2.099d0334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263d344f10a4fbcb5081",
+    "url": "/midterm-liveweb-final/static/js/main.eeb55778.chunk.js"
+  },
+  {
+    "revision": "ca093b0e6386e66d0e21",
+    "url": "/midterm-liveweb-final/static/js/runtime-main.8556752e.js"
+  }
+]);
--- src/Socket.js
+++
@@ -1,6 +0,0 @@
-import { io } from 'socket.io-client';
-
-// const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3001';
-
-export const socket = io(); // Export the 'socket' variable
-
